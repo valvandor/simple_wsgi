b409d6d8e535a8632c3635e98e0dde995686dc4a
Sincov Vitalii <sincov@algont.ru>
2022-12-26 20:36:35 +0300

    refactor: allocate logic to start application in framework

--- framework/main.py
+++ framework/main.py
@@ -2,12 +2,25 @@
 Module is responsible for communication between WSGI-server and python application
 """
 from typing import List
+from wsgiref import simple_server
 
 
 class WSGIFramework:
     """
     Base class of WSGI-framework
     """
+    def __init__(self, config: dict):
+        self._config = config
+
+    @property
+    def _port(self) -> int:
+        assert 'port' in self._config
+        return self._config['port']
+
+    @property
+    def _root_path(self) -> str:
+        assert 'root_path' in self._config
+        return self._config['root_path']
 
     def __call__(self, environ: dict, start_response) -> List[bytes]:
         """
@@ -24,3 +37,18 @@ class WSGIFramework:
         start_response(status, headers)
         body = 'Hello from wsgi-framework'
         return [body.encode('utf-8')]
+
+    def run(self):
+        """
+        Entry point for running WSGI-application
+        """
+
+        print(f"Serving {self._root_path} on port {self._port}, ctrl+c to stop")
+        httpd = simple_server.make_server("", self._port, self)
+
+        print('Check for a link: {}'.format(f"http://localhost:{self._port}/wsgi/1.cgi.app.py"))
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("Shutting down.")
+            httpd.server_close()
--- run.py
+++ run.py
@@ -1,20 +1,12 @@
 import os
-from wsgiref import simple_server
 from framework.main import WSGIFramework
 
 
 if __name__ == "__main__":
-    # create an object for the WSGI application
-    application = WSGIFramework()
-
-    port = 8000
-    root_path = os.getcwd()
-
-    httpd = simple_server.make_server("", port, application)
-    print(f"Serving {root_path} on port {port}, ctrl+c to stop")
-    print('Check for a link: {}'.format(f"http://localhost:{port}/wsgi/1.cgi.app.py"))
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        print("Shutting down.")
-        httpd.server_close()
+    # create an object for the WSGI application based on config
+    config = {
+        'root_path': os.getcwd(),
+        'port': 8000
+    }
+    application = WSGIFramework(config)
+    application.run()
